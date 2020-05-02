78cdeb1f5887942259d6877566e0df324ab607f3
low101043 <nathaniel@lowis.co.uk>
2020-05-02 21:14:46 +0100

    Adding info

--- src/main/java/com/natlowis/ai/search/informed/AStar.java
+++ src/main/java/com/natlowis/ai/search/informed/AStar.java
@@ -1,5 +1,36 @@
 package com.natlowis.ai.search.informed;
 
-public class AStar {
+import com.natlowis.ai.graphs.Connection;
+import com.natlowis.ai.graphs.Graph;
+import com.natlowis.ai.search.uninformed.SearchAlgorithm;
 
+public class AStar implements SearchAlgorithm {
+	
+	Graph graph;
+	
+	
+	public AStar(Graph graph) {
+		this.graph = graph;
+	}
+
+	@Override
+	public void algorithmToImplement(int startNode, int endNode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Integer[] nodesToVisit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Connection[] solutionActions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public 
 }
